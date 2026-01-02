388608e2087776f15189da1c4c9a39013085c358
coderija <coderija@gmail.com>
2026-01-02 17:43:08 -0500

    uncomment import

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -15,7 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 # from .populate import initiate
 
-
+ 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
